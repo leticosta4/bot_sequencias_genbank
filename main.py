71cc9779c9a7fb16962c736c3c996d9793f83d6d
let <leticiacostaoa@gmail.com>
2025-12-31 17:08:59 -0300

    style: add black for code formatting

--- main.py
+++ main.py
@@ -3,21 +3,33 @@ from scripts.setup import arbovirus_search
 from scripts.files_config import prepare_directories, gerar_txt
 
 seq_amount = 0
-lista_arbovirus = ['dengue virus type 1', 'dengue virus type 2', 'dengue virus type 3', 'dengue virus type 4', 'chikungunya virus', 'zika virus', "oropouche virus"]
+lista_arbovirus = [
+    "dengue virus type 1",
+    "dengue virus type 2",
+    "dengue virus type 3",
+    "dengue virus type 4",
+    "chikungunya virus",
+    "zika virus",
+    "oropouche virus",
+]
 
-for i in range(len(lista_arbovirus)): 
+for i in range(len(lista_arbovirus)):
     print(f"{i+1} - {lista_arbovirus[i]} ")
 
 op = 1
-while(True):
-    op = int(input("\nveja a lista acima e escolha o arbovírus cujas sequencias deseja fazer o download (para sair digite 8) >> "))
+while True:
+    op = int(
+        input(
+            "\nveja a lista acima e escolha o arbovírus cujas sequencias deseja fazer o download (para sair digite 8) >> "
+        )
+    )
     if op > 8 or op < 1:
         print("o valor inserido deve ser de 1 a 8. Tente novamente\n")
     elif op == 8:
         print("\n\nvoce cancelou o funcionamento do bot.")
-        break 
+        break
     else:
-        arbovirus = lista_arbovirus[op-1].replace(' ', '+')
+        arbovirus = lista_arbovirus[op - 1].replace(" ", "+")
 
         absolute_path = prepare_directories()
 
@@ -25,9 +37,11 @@ while(True):
         seq_amount = arbovirus_search(arbovirus, absolute_path)
         end_time = time.time()
         duration_downloads = end_time - begin_time
-        if(seq_amount != -1): gerar_txt(lista_arbovirus[op-1], seq_amount, duration_downloads)
-        else: 
-            #talvez fazer uma verificação de timeout ou colocar em um try-catch
-            print("Arquivo de informações não gerado devido a problema no download da sequência")
+        if seq_amount != -1:
+            gerar_txt(lista_arbovirus[op - 1], seq_amount, duration_downloads)
+        else:
+            # talvez fazer uma verificação de timeout ou colocar em um try-catch
+            print(
+                "Arquivo de informações não gerado devido a problema no download da sequência"
+            )
         break
-
--- scripts/setup.py
+++ scripts/setup.py
@@ -9,13 +9,14 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 
-#preparando o driver
+
+# preparando o driver
 def driver_setup(download_directory):
     options = ChromeOptions()
-    #options.add_argument("--headless") 
-    #options.add_argument('--disable-popup-blocking') 
+    # options.add_argument("--headless")
+    # options.add_argument('--disable-popup-blocking')
 
-    #preferencias para o download 
+    # preferencias para o download
     preferences = {
         "download.default_directory": f"{download_directory}",
         "download.prompt_for_download": False,
@@ -28,30 +29,44 @@ def driver_setup(download_directory):
     service = ChromeService(executable_path=ChromeDriverManager().install())
     d = webdriver.Chrome(service=service, options=options)
 
-    d.implicitly_wait(5) 
+    d.implicitly_wait(5)
     return d
+
 
 def arbovirus_search(query, download_directory):
     driver = driver_setup(download_directory)
 
-    if(query == "oropouche+virus"): query = "oropouche virus AND isolate AND segment"
+    if query == "oropouche+virus":
+        query = "oropouche virus AND isolate AND segment"
     print(query)
     search_url = f"https://www.ncbi.nlm.nih.gov/nuccore/?term={query}"
 
-    driver.get(search_url) 
+    driver.get(search_url)
 
-    #selecionando campos especificos para as sequencias e achando o botao p baixar as seq
+    # selecionando campos especificos para as sequencias e achando o botao p baixar as seq
     driver.find_element(By.CSS_SELECTOR, "#seqsendto > a:nth-child(1)").click()
     driver.find_element(By.CSS_SELECTOR, "#complete_rec").click()
     driver.find_element(By.CSS_SELECTOR, "#dest_File").click()
 
-    seq_num = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#submenu_File_hint"))).text
+    seq_num = (
+        WebDriverWait(driver, 10)
+        .until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#submenu_File_hint")))
+        .text
+    )
 
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#file_format > option:nth-child(7)"))).click()
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#file_sort > option:nth-child(2)"))).click()
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "#file_format > option:nth-child(7)")
+        )
+    ).click()
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "#file_sort > option:nth-child(2)")
+        )
+    ).click()
     driver.find_element(By.CSS_SELECTOR, "#submenu_File > button:nth-child(3)").click()
 
-    while(True):
+    while True:
         # a cada 3 segundos vai verificar se o download dos arquivos encerrou ('sequence.gbc.xml' no output)
         time.sleep(3)
         if download_verification():
